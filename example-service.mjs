d6468a703c50f8c5d1556461ce01f94b034c94be
maxmcd <max.t.mcdonnell@gmail.com>
2024-07-21 18:06:07 +0000

    print requests

--- example-service.mjs
+++ example-service.mjs
@@ -1,6 +1,13 @@
 import http from "node:http";
 const server = http
-  .createServer((_, res) => setTimeout(() => res.end("Hello World"), 1000))
+  .createServer((req, res) => {
+    console.log(JSON.stringify([req.url, req.headers]));
+    if (req.url.includes("health")) {
+      res.end("OK");
+      return;
+    }
+    setTimeout(() => res.end("Hello World"), 1000);
+  })
   .listen(process.env.PORT || 8000)
   .on("listening", () => {
     console.log(`Server listening on port ${server.address().port}`);
